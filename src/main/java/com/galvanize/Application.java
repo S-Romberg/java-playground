9cc3f5f72cf31414d0975b75e63612031b212961
S-Romberg <spencer.romberg@gmail.com>
2018-11-11 13:19:16 -0700

    if/else added

--- src/main/java/com/galvanize/Application.java
+++ src/main/java/com/galvanize/Application.java
@@ -4,6 +4,15 @@ public class Application {
 
     public static void main(String[] args) {
         System.out.println("Hello world");
+
+         String foo = "bar";
+         int x = 7;
+         int y = 5;
+         if( x < 0) {
+             System.out.println(x + y);
+         } else {
+             System.out.println("oh no");
+         }
     }
 
 
